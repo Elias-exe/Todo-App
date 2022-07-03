6a681acc754274cfac019eb29e431ff355a30799
Elias-exe <adventurergamer11@hotmail.com>
2022-07-03 20:03:50 -0300

    feat: organizing the project and adding Styled Components

---
+++ src/components/app/index.js
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components';
+import { Container } from './styles';
+import defaultTheme from '../../assets/styles/themes/default';
+import GlobalStyles from '../../assets/styles/global';
+import Home from '../home/index';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Container>
+        <Home />
+      </Container>
+    </ThemeProvider>
+
+  );
+}
