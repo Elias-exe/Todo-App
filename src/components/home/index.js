a9f388d9cfe747d05f83e91f264de5de33a1a147
Elias-Exe <adventurergamer11@hotmail.com>
2022-08-15 21:08:18 -0300

    feat: Implementation todo list

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,14 +1,59 @@
-import { Container } from './styles';
+import { useState, useEffect, useCallback } from 'react';
+import { Container, SearchBarContainer } from './styles';
 import Header from '../header';
 import NavBar from '../navBar';
-import SearchBar from '../searchBar';
 
 export default function Home() {
+  const [todoList] = useState([
+    { id: Math.random(), tarefa: 'aaa' },
+  ]);
+  const [todoName, setTodoName] = useState('');
+  const [, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
+
+  function handleChangeText(event) {
+    setTodoName(event.target.value);
+  }
+
+  function handleNewTodo() {
+    todoList.push({ id: Math.random(), tarefa: todoName });
+    forceUpdate();
+  }
+
+  function handleChangeTodoStatus(event) {
+    if (event.target.checked) {
+      console.log(`ID:${event.target.id}`);
+    }
+  }
+
+  useEffect(() => {
+
+  }, [todoList]);
+
   return (
     <Container>
       <Header />
       <NavBar />
-      <SearchBar />
+      <SearchBarContainer>
+        <input type="text" placeholder="add details" onChange={handleChangeText} />
+        <button type="button" onClick={handleNewTodo}>Add</button>
+      </SearchBarContainer>
+      <div className="todoContainer">
+        {todoList.map((todo) => (
+          <label htmlFor={todo.id} key={todo.id}>
+            <input
+              type="checkbox"
+              id={todo.id}
+              name={todo.id}
+              onClick={handleChangeTodoStatus}
+            />
+            {todo.tarefa}
+            <br />
+          </label>
+
+        ))}
+      </div>
+
     </Container>
 
   );
